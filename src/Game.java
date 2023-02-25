a63b853b4d65f52354b031dbc0fd1e1d0571ba32
cxrdary <claycordary@gmail.com>
2023-02-24 20:28:15 -0800

    Spring 2023 2/24 After much more research I figured out what was wrong and corrected it.

--- src/Game.java
+++ src/Game.java
@@ -10,25 +10,24 @@ import java.util.Random;
 import java.util.Scanner;
 
 class Dice {
-    private int sideFacingUp;
-    private int sides;
-    private Random randomGenerator;
-    Dice d1 = new Dice(6);
-    Dice d2 = new Dice(20);
+    private static int sideFacingUp;
+    private static int sides;
+    private static Random randomGenerator;
 
-    public void Roll(int side) {
-        sideFacingUp = randomGenerator.nextInt(1, sides);
+    public void Roll() {
+        this.sideFacingUp = randomGenerator.nextInt(this.sides) + 1;
     }
 
-    public void view() {
-        System.out.println(sideFacingUp);
+    public int view() {
+        return this.sideFacingUp;
     }
 
     public Dice(int sides) {
         this.sides = sides;
+        this.randomGenerator = new Random();
     }
 
-    class Player {
+    public static class Player {
         private String name;
         private int score;
 
@@ -45,26 +44,26 @@ class Dice {
         }
 
         public void toss(Dice die) {
-            Roll(die.sides);
-            score = sideFacingUp;
+            die.Roll();
+            this.score += die.view();
         }
     }
 
-    public class Game {
+    public static class Game {
         Player p1;
         Player p2;
         Dice die;
-      public Game(Player pa1, Player pa2, Dice daie) {
-          p1 = pa1;
-          p2 = pa2;
-          die = daie;
+      public Game(Player p1, Player p2, Dice die) {
+          this.p1 = p1;
+          this.p2 = p2;
+          this.die = die;
       }
-        public void play() {
+        public String play() {
             Player current = this.p1;
             takeTurn(current);
             nextPlayer(current);
             takeTurn(current);
-            announceWinner();
+            return this.announceWinner();
         }
 
 
@@ -82,31 +81,27 @@ class Dice {
         }
 
         public String announceWinner () {
-            int p1score = p2.getScore();
-            int p2score = p1.getScore();
-            System.out.println("Player 1 had a score of " + p1score);
-            System.out.println("Player 2 had a score of " + p2score);
-            if (p1score > p2score) {
-                System.out.println("This means that player 1 is our winner!");
-            } else if (p2score > p1score) {
-                System.out.println("This means that player 2 is our winner!");
+            System.out.printf("%s scored %d\n", this.p1.getName(), this.p1.getScore());
+            System.out.printf("%s scored %d\n", this.p2.getName(), this.p2.getScore());
+            if (this.p1.getScore() > this.p2.getScore()) {
+                return this.p1.getName();
             } else {
-                System.out.println("it looks like we have a tie!");
+                return this.p2.getName();
             }
-            return null;
+
         }
     }
 
         public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);
-            System.out.println("How many sides should the die have?");
-            int diceSides = scanner.nextInt();
-            Dice gameDie = new Dice(diceSides);
-            Player p1;
-            Player p2;
-            Game(p1, p2, gameDie);
-            play();
-
+            System.out.print("Enter number of sides: ");
+            int sides = scanner.nextInt();
+            Dice die = new Dice(sides);
+            Player p1 = new Player("Player 1");
+            Player p2 = new Player("Player 2");
+            Game game = new Game(p1, p2, die);
+            String winner = game.play();
+            System.out.printf("%s wins!\n", winner);
             }
         }
 
